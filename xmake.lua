934f31fc765dc9fc7ab2463b4b49d9ff279a6fe1
Stehsaer <stehsaer@outlook.com>
2025-09-01 17:19:48 +0800

    Minor changes - Added extra new line before and after Size Report message - Force minsizerel to contain extra debug symbol file

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,10 @@
 add_rules("mode.debug", "mode.release", "mode.minsizerel", "mode.releasedbg")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
 
+if is_mode("minsizerel") then
+	set_symbols("debug")
+end
+
 includes("toolchain.lua", "rule.lua")
 
 set_toolchains("riscv-gcc-baremetal")
